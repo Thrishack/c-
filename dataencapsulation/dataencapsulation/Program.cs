0a649b762c64909a84d2ba7411fc27d6b980e959
Thrisha <thrishag03@gmail.com>
2025-09-19 14:21:10 +0530

    Add files via upload

---
+++ dataencapsulation/dataencapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataEncapsulationDemo;
+using System;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Name = "bob";
+
+        person.Age = 55;
+
+        Console.WriteLine($"The name is {person.Name} and age is {person.Age}");
+
+    }
+}
+
+
