e633a4915044f21ce9bb754a253f1956ece066ba
Thrisha <thrishag03@gmail.com>
2025-09-19 14:27:52 +0530

    Add files via upload

---
+++ greet/greet/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GreetPerson
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name:");
+            String name = Console.ReadLine();
+            Greet g = new Greet(name);
+            g.greet();
+        }
+    }
+}
---
+++ greet/greet/greet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetPerson
+{
+    public class Greet
+    {
+
+
+        String name;
+        public Greet()
+        {
+            name = " Kartik ";
+        }
+
+        public Greet(string name)
+        {
+            this.name = name;
+        }
+
+        public void greet()
+        {
+            Console.WriteLine($"Good morning,{name}!");
+        }
+    }
+}
+
+
