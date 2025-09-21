06953e51c5d27fca26b5604e53d3aabdf66b0cd6
Thrisha <thrishag03@gmail.com>
2025-09-21 12:30:36 +0530

    Add files via upload

---
+++ posnegzero/posnegzero/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosNegZero
+{
+    public class PNZ
+    {
+        int num;
+
+        public PNZ()
+        {
+            num = 2;
+        }
+
+        public PNZ(int num)
+        {
+            this.num = num;
+        }
+
+        public void check()
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("The number is 0");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("The number is positive");
+            }
+            else
+            {
+                Console.WriteLine("The number is negative");
+
+            }
+        }
+
+    }
+}
---
+++ posnegzero/posnegzero/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PosNegZero
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            PNZ pnz = new PNZ(num);
+            pnz.check();
+        }
+    }
+}
